e0e3dde75ad813c8e620df04b7a4da7c7a08e61d
SatoruSaito <satorusaito1108@gmail.com>
2024-12-24 14:51:26 -0800

    fixed warning

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 <h1 align="center"> ReactJs Online Store </h1> <br>
 
-
 <h3 align="center">
   A Simple Online Shop to Sell T-shirts and mark them as favorite. Built with Reactjs.
 </h3>
@@ -21,18 +20,14 @@ ReactJs Online Store is a simple online shop to sell t-shirts and mark pictures 
 
 A few of the things you can do with this app:
 
-* View all Products
-* View Product Detail
-* Add Product to Cart
-* Delete Product from Cart
-* Mark/Unmark Product as Favorite
-* Place an Order
+- View all Products
+- View Product Detail
+- Add Product to Cart
+- Delete Product from Cart
+- Mark/Unmark Product as Favorite
+- Place an Order
 
 ## Installation Process
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-**Installation Process (Windows)**
 
 1. Check if you have nodejs installed `node -v`
 2. Clone This Project `git clone https://github.com/mahmud-sajib/ReactJs-Online-Store.git`
@@ -69,9 +64,3 @@ Live Project url: [reactjs-online-store](https://reactjs-online-store.netlify.ap
 <p>
   <img src = "https://i.imgur.com/scuAjAI.png" width=700>
 </p>
-
-## Feedback
-
-Feel free to [file an issue](https://github.com/mahmud-sajib/ReactJs-Online-Store/issues/new). If you wish to contribute, please feel free to do so!
-
- 
--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer';
-import ProductDetail from './components/Product/ProductDetail';
-import Store from './pages/Store';
-import About from './pages/About';
-import Cart from './pages/Cart';
-import Wishlist from './pages/Wishlist';
-import {Switch, Route} from "react-router-dom"
-import './style.css'
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import ProductDetail from "./components/Product/ProductDetail";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
+import { Switch, Route } from "react-router-dom";
+import "./style.css";
 
 function App() {
   return (
     <div>
-        <Header />
-        
-        <Switch>
-          <Route exact path="/">
-            <Store />
-          </Route>
+      <Header />
 
-          <Route path="/about">
-            <About />
-          </Route>
+      <Switch>
+        <Route exact path="/">
+          <Store />
+        </Route>
 
-          <Route path="/cart">
-            <Cart />
-          </Route>
+        <Route path="/about">
+          <About />
+        </Route>
 
-          <Route path="/wishlist">
-            <Wishlist />
-          </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
 
-          <Route path="/product/:productId">
-            <ProductDetail />
-          </Route>
-        </Switch>
-        
-        <Footer />
+        <Route path="/wishlist">
+          <Wishlist />
+        </Route>
+
+        <Route path="/product/:productId">
+          <ProductDetail />
+        </Route>
+      </Switch>
+
+      <Footer />
     </div>
   );
 }
